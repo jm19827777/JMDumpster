bc16b5f6de3cd2a520f9c2165db4d0e98eeb62b9
jm19827777 <59941083+jm19827777@users.noreply.github.com>
2020-01-15 18:30:59 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/************************************************************************* 
+* Hello.java
+************************************************************************** 
+* Merritt James
+* 15JAN2020
+* CMSC255 Section 901
+************************************************************************/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Merritt James>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1>");
+		System.out.println("<15JAN2020>");
+		System.out.println("<CMSC255 901>");
+		System.out.println();
+		}
+}
